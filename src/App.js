6d733ac66bdd9e8b24ed47013920b24bfbee8a81
klaus-moser <klaus-moser@posteo.de>
2022-05-22 19:00:17 +0200

    (no branch): Auto stash before rebase of "featue/basis"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import Header from './components/Header';
-import Footer from './components/Footer';
-
 
 function App() {
   return (
     <div className='container'>
       <Header />
-      <Footer />
     </div>
   );
 }
