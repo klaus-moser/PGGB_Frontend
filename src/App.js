905a7ead6fe5e891697c7af7e76b766f45cc2b6e
klaus-moser <klaus-moser@posteo.de>
2022-05-22 20:49:24 +0200

    Basic Gallery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import { Container } from "react-bootstrap";
-import Karten from './components/Karten';
+import Karten from "./components/Karten";
 import Header from "./components/Header";
+import KartenData from "./components/KartenData";
 
 function App() {
+  const [karte, setKarte] = useState(KartenData);
+
   return (
     <Container>
       <Header />
-      <Karten />
+      <Karten karte={karte} />
     </Container>
   );
 }
--- src/components/Karten.js
+++ src/components/Karten.js
@@ -1,18 +1,19 @@
 import { Container, Col, Row } from "react-bootstrap";
-import Karte from './Karte';
+import Karte from "./Karte";
 
-function Karten() {
+function Karten({ karte }) {
+  if (!karte || karte.length === 0) {
+    return <p>No Memes yet!</p>;
+  }
+
   return (
     <Container>
       <Row>
-        <Col sm><Karte titel={'Bertel 1'} dir={'test_images/1.jpg'}/></Col>
-        <Col sm><Karte titel={'Bertel 2'} dir={'test_images/2.jpg'}/></Col>
-        <Col sm><Karte titel={'Bertel 3'} dir={'test_images/3.jpg'}/></Col>
-      </Row>
-      <Row>
-        <Col sm><Karte titel={'Bertel 4'} dir={'test_images/4.jpg'}/></Col>
-        <Col sm><Karte titel={'Bertel 5'} dir={'test_images/5.jpg'}/></Col>
-        <Col sm><Karte titel={'Bertel 6'} dir={'test_images/6.jpg'}/></Col>
+        {karte.map((item) => (
+          <Col sm>
+            <Karte titel={item.title} dir={item.dir} />
+          </Col>
+        ))}
       </Row>
     </Container>
   );
