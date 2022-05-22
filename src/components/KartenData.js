2a00c77e79a89591adf884480a1396a8861dcaeb
klaus-moser <klaus-moser@posteo.de>
2022-05-22 20:49:16 +0200

    Add KartenData

---
+++ src/components/KartenData.js
@@ -0,0 +1,10 @@
+const KartenData = [
+  { id: 1, title: "Bertel 1", dir: "test_images/1.jpg" },
+  { id: 2, title: "Bertel 2", dir: "test_images/2.jpg" },
+  { id: 3, title: "Bertel 3", dir: "test_images/3.jpg" },
+  { id: 4, title: "Bertel 4", dir: "test_images/4.jpg" },
+  { id: 5, title: "Bertel 5", dir: "test_images/5.jpg" },
+  { id: 6, title: "Bertel 6", dir: "test_images/6.jpg" },
+];
+
+export default KartenData
