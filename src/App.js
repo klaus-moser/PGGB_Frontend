f22d62b03d83738c382c1331ed336b853433e041
klaus-moser <klaus-moser@posteo.de>
2022-05-22 12:52:53 +0200

    Add simple footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className='container'>
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <Footer>
+      <div className='col'>
+        Impressum
+      </div>
+    </Footer>
+  )
+}
+
+export default Footer
