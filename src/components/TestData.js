4b438ac035c331fd5916f090dc6d3729fbadef37
klaus-moser <klaus-moser@posteo.de>
2022-06-16 13:44:06 +0200

    Adapt test data

--- src/components/TestData.js
+++ src/components/TestData.js
@@ -5,6 +5,14 @@ const TestData = [
   { id: 4, title: "Bertel 4", dir: "test_images/4.jpg" },
   { id: 5, title: "Bertel 5", dir: "test_images/5.jpg" },
   { id: 6, title: "Bertel 6", dir: "test_images/6.jpg" },
+  { id: 7, title: "Bertel 7", dir: "test_images/7.jpg" },
+  { id: 8, title: "Bertel 8", dir: "test_images/8.jpg" },
+  { id: 9, title: "Bertel 9", dir: "test_images/9.jpg" },
+  { id: 10, title: "Bertel 10", dir: "test_images/10.jpg" },
+  { id: 11, title: "Bertel 11", dir: "test_images/11.jpg" },
+  { id: 12, title: "Bertel 12", dir: "test_images/12.jpg" },
+  { id: 13, title: "Bertel 13", dir: "test_images/13.jpg" },
+  { id: 14, title: "Bertel 14", dir: "test_images/14.jpg" },
 ];
 
 export default TestData
