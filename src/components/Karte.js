0d59c4bfd036bb14053aa035df1bc3eb580107c9
klaus-moser <klaus-moser@posteo.de>
2022-06-16 14:38:26 +0200

    New card style

--- src/components/Karte.js
+++ src/components/Karte.js
@@ -1,23 +1,31 @@
 import PropTypes from "prop-types";
 import { Card } from "react-bootstrap";
 
-function Karte({ titel, dir }) {
+function Karte({ title, dir, text }) {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={dir} />
-      <Card.Body>
-        <Card.Title>{titel}</Card.Title>
-      </Card.Body>
+
+    <Card style={{ width: "18rem" }} className="bg-dark text-white">
+      <Card.Img variant="top" src={ dir } alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Title>{ title }</Card.Title>
+        <Card.Text>
+          This is a wider card with supporting text below as a natural lead-in to
+          additional content. This content is a little bit longer.
+        </Card.Text>
+        <Card.Text>Last updated 3 mins ago</Card.Text>
+      </Card.ImgOverlay>
     </Card>
   );
 }
 
 Karte.defaultProps = {
-  text: "#Title",
+  title: "#title",
+  text: "#text",
 };
 
 Karte.propTypes = {
-  titel: PropTypes.string,
+  title: PropTypes.string,
+  text: PropTypes.string,
   dir: PropTypes.string,
 };
 
