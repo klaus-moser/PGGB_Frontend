6c1db240b0db4029108b83d99471c5943c7a638e
klaus-moser <klaus-moser@posteo.de>
2022-06-19 12:07:56 +0200

    Mini bug: wrong path for text grey

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ function Navbar() {
             />
             <p
               className={
-                pathMatchRoute("/")
+                pathMatchRoute("/profile")
                   ? "navbarListItemNameActive"
                   : "navbarListItemName"
               }
